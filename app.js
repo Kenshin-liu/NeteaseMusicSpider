185543c9cd44ae648e38c0d66f3b51acd7936514
liudiguang <zronediguang@gmail.com>
2018-04-17 23:54:46 +0800

    '暂存，待解决网易接口请求次数过多返回错误问题'

--- app.js
+++ app.js
@@ -4,27 +4,30 @@ const { query } = require('./db')
 const { get } = require('./util/http')
 const { list, initial, api, filter } = require('./config/singer')
 
-function collect() {
+async function collect() {
   list.forEach(item => {
     const { style, category, id } = item
-    initial.forEach(async index => {
-      try {
-        const response = await get(`http://music.163.com/discover/artist/cat`, { id: id, initial: index })
-        const $ = cheerio.load(response.data)
-        const elements = $('li .s-fc0')
-        Object.keys(elements).forEach(async el => {
-          if (filter.indexOf(el) > 0) return
-          const catId = $(elements[el]).attr('href') ? $(elements[el]).attr('href').replace(/[^0-9]/ig, "") : ''
-          const name = $(elements[el]).text()
-          if (!catId) return
-          let sql = 'insert into singer(name, style, initial, category, catId) values(?,?,?,?,?)'
-          let dataList = await query(sql, [name, style, index, category, catId])
-        })
-      } catch (e) {
-        console.log(e)
-      }
+    initial.forEach(index => {
+      await getElements(id, index)
     })
   })
 }
 
-collect()
+async function getElements(id, index) {
+  const response = await get(`http://music.163.com/discover/artist/cat`, { id: id, initial: index })
+  const $ = cheerio.load(response.data)
+  const elements = $('li .s-fc0')
+  Object.keys(elements).forEach(async el => {
+    await setQuery(el)
+  })
+}
+
+async function setQuery(el) {
+  const catId = $(elements[el]).attr('href') ? $(elements[el]).attr('href').replace(/[^0-9]/ig, "") : ''
+  const name = $(elements[el]).text()
+  if (!catId || filter.indexOf(el) > 0) return
+  let sql = 'insert into singer(name, style, initial, category, catId) values(?,?,?,?,?)'
+  let dataList = await query(sql, [name, style, index, category, catId])
+}
+
+collect().catch(error => console.log(error))
--- song.js
+++ song.js
@@ -7,28 +7,27 @@ async function selectAllData() {
   return dataList
 }
 
-async function getData() {
+async function getHotSongs() {
   const datas = await selectAllData()
   datas.forEach(async singer => {
     const songs = await get('http://localhost:3000/artists', { id: singer.catId })
-    await setData(songs.data.hotSongs, singer)
+    await getSong(songs.data.hotSongs, singer)
   })
 }
 
-async function setData(hotSongs, singer) {
+async function getSong(hotSongs, singer) {
   hotSongs.forEach(async song => {
     const comments = await get('http://localhost:3000/comment/music', { id: song.id, limit: 1 })
     const total = comments.data.total
-    console.log(total)
     if (total < 5000) return
-    await setMysql(total, song, singer).catch(error => console.log(error))
+    await getComment(total, song, singer).catch(error => console.log(error))
   })
 }
 
-async function setMysql(total, song, singer) {
+async function getComment(total, song, singer) {
   const { id, name, style, category, catId } = singer
   let sql = 'insert into song(singer, singerId, style, songId, name, comment) values(?,?,?,?,?,?)'
   let result = await query(sql, [name, id, style, song.id, song.name, total])
 }
 
-getData().catch(error => console.log(error))
+getHotSongs().catch(error => console.log(error))
