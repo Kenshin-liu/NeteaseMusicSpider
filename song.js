0f43660cc8d1860d419c44d7bdff8a9594924d91
liudiguang <zronediguang@gmail.com>
2018-04-20 00:29:21 +0800

    '完善歌手以及歌曲信息的抓取'

--- song.js
+++ song.js
@@ -2,37 +2,37 @@ const { query } = require('./db')
 const { get } = require('./util/http')
 
 async function selectAllData() {
-  const sql = 'select * from singer limit 300'
+  const sql = 'select * from singer'
   const dataList = await query(sql)
   return dataList
 }
 
 async function getHotSongs() {
   let errorId = ''
-  try {
-    const datas = await selectAllData()
-    for (let singer of datas) {
+  const datas = await selectAllData()
+  for (let singer of datas) {
+    try {
       const songs = await get('http://localhost:3000/artists', { id: singer.catId })
       errorId = singer.catId
       await getSong(songs.data.hotSongs, singer)
+    } catch (e) {
+      console.log('error singerId:' + errorId)
     }
-  } catch (e) {
-    console.log('error singerId:' + errorId)
   }
 }
 
 async function getSong(hotSongs, singer) {
   let errorId = ''
-  try {
-    for (let song of hotSongs) {
+  for (let song of hotSongs) {
+    try {
       errorId = song.id
       const comments = await get('http://localhost:3000/comment/music', { id: song.id, limit: 1 })
       const total = comments.data.total
-      if (total < 500) return
+      if (total < 10000) return
       await getComment(total, song, singer).catch(error => console.log(error))
+    } catch (e) {
+      console.log('error songId:' + errorId)
     }
-  } catch (e) {
-    console.log('error singerId:' + errorId)
   }
 }
 
